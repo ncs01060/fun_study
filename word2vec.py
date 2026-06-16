11786443707656d605eabe98d6520c5e4967c749
ncs01060 <ncs01045@gmail.com>
2026-06-16 19:24:18 +0900

    전처리 추가

--- word2vec.py
+++ word2vec.py
@@ -1,3 +1,20 @@
-import numpy as np
-import pandas
+import re
+from lxml import etree
+from nltk.tokenize import word_tokenize, sent_tokenize
 
+path = "./data"
+targetText = etree.parse(open(path+"data.xml","r",encoding="UTF-8"))
+parseText = "\n".join(targetText.xpath("//content/text()"))
+
+# 데이터 전처리
+contentText = re.sub('\([^)]*\)', '', parseText)
+# 문장 단위 토큰화 
+sentText = sent_tokenize(contentText)
+
+
+# 대문자 => 소문자, 구두점 제거 (영문, 숫자 제외)
+normalizedText = [] 
+for sent in sentText: 
+    tokens = re.sub("[^a-z0-9]+"," ",sent.lower())
+    normalizedText.append(tokens)
+result = [ word_tokenize(s) for s in normalizedText ]
