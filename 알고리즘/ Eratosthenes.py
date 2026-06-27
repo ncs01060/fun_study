309fe50625099059b0f36f7a1c0a4e9a2b4cde38
ncs01060 <ncs01045@gmail.com>
2026-06-27 14:23:41 +0900

    소수 합

--- 알고리즘/ Eratosthenes.py
+++ 알고리즘/ Eratosthenes.py
@@ -10,7 +10,11 @@ for i in range(2,int(math.sqrt(num))+1):
     if not isPrime[i]: continue
     for j in range(i*i,num+1,i):
         isPrime[j] = False
+    
 
-print(isPrime.count(True)-2)
+for i in range(2, num + 1):
+    if isPrime[i]:
+        primeNumber.append(i)
+print(sum(primeNumber))
 
 ## 블로그에 적어야지
