2dea72b4399757d1910a589a301e4635e7eb3798
ncs01060 <ncs01045@gmail.com>
2026-07-06 23:35:56 +0900

    타 알고리즘과 시간 비교

--- 알고리즘/sampleGCD.py
+++ 알고리즘/sampleGCD.py
@@ -1,13 +1,29 @@
-import time
+import time,math
 def gcd(a:int,b:int):
-    s = a%b
-    if s != 0:
-        return gcd(b,s)
-    else:
-        return b
+    while b != 0:
+        a,b = b, a%b
+    return a
 
 a,b = map(int,input().split())
+
 start = time.time()
 print(gcd(a,b))
+end = time.time()
+print(f"{end - start:.5f}")
+
+
+start = time.time()
+# n부터 1까지 반복
+for i in range(a, 0, -1):
+    # a와 b가 동시에 나누어떨어지는 첫 번째 수가 최대 공약수
+    if a % i == 0 and b % i == 0:
+        print(i)
+        # 최대 공약수를 구했기 때문에 빠져나옴
+        break
+end = time.time()
+print(f"{end - start:.5f}")
+
+start = time.time()
+print(math.gcd(a,b))
 end = time.time()
 print(f"{end - start:.5f}")
